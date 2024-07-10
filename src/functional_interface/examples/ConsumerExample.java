f85cadcd33d5b81af88483ff71aad0cf4e0c76da
Edson Mizuma <edson.mizuma@gmail.com>
2024-07-10 11:04:15 -0300

    Resolvendo exercício PredicateExample e aprimorando clase ConsumerExample

--- src/functional_interface/examples/ConsumerExample.java
+++ src/functional_interface/examples/ConsumerExample.java
@@ -3,6 +3,7 @@ package functional_interface.examples;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 /**
  * Representa uma operação que aceita um argumento do tipo T e não retorna nenhum resultado.
@@ -21,13 +22,23 @@ public class ConsumerExample {
             }
         };
 
-        //Usar o Consumer para imprimir números pares no Stream
-        numeros.forEach(num -> {
+        // Usar o Consumer para imprimir números pares no Stream
+        numeros.stream()
+                .filter(n -> n % 2 == 0)
+                .forEach(System.out::println);
+    }
+
+    ;
+
+
+
+        /*numeros.forEach(num -> {
                     if (num % 2 == 0) {
                         System.out.println(num);
                     }
                 }
-        );
 
-    }
+         */
+
 }
+
