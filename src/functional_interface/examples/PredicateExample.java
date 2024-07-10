05bbfc57c3d5b4f5480e121ddd8791244cbe5b8d
Edson Mizuma <edson.mizuma@gmail.com>
2024-07-10 10:03:17 -0300

    Resolvendo exercício da SupplierExample

---
+++ src/functional_interface/examples/PredicateExample.java
@@ -0,0 +1,12 @@
+package functional_interface.examples;
+
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Representa uma operação que não aceita nenhum argumento e retorna um resultado do tipo T. É comumente
+ */
+
+public class PredicateExample {
+}
